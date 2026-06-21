ab1a1f5417f69ed91b5067d6fe29b09cb48d3402
ねんねこ <95109758+nennneko5787@users.noreply.github.com>
2026-06-21 18:39:19 +0900

    Add proxy support to AsyncClient configuration

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ def convert(text):
 http = httpx.AsyncClient(
     timeout=None,
     follow_redirects=True,
+    proxy=os.getenv("proxy"),
     headers={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36",
     },
